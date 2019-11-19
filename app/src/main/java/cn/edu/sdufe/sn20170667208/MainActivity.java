3458c63b1b66084b98bfb61da1522d2e14ee8236
921504543@qq.com <921504543@qq.com>
2019-11-19 17:31:45 +0800

    this is my first android

---
+++ app/src/main/java/cn/edu/sdufe/sn20170667208/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.sdufe.sn20170667208;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
